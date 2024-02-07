53e5bafc15c93a3b89f66754d1c05e54d633c541
이재호 <jhlee940801@gmail.com>
2024-02-07 17:50:37 +0900

    1-11

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,29 +1,49 @@
 package io.security.basicsecurity;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
   @Autowired
   UserDetailsService userDetailsService;
+
+  @Bean
+  public UserDetailsManager setInMemoryUsers () {
+    UserDetails user = User
+      .builder()
+      .username("user")
+      .password("{noop}1234")
+      .roles("USER")
+      .build();
+    
+    UserDetails sys = User
+      .builder()
+      .username("sys")
+      .password("{noop}1234")
+      .roles("SYS")
+      .build();
+
+    UserDetails admin = User
+      .builder()
+      .username("admin")
+      .password("{noop}1234")
+      .roles("ADMIN", "SYS", "USER")
+      .build();
+
+    return new InMemoryUserDetailsManager(user, sys, admin);
+  }
 
   @Bean
   public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
@@ -44,41 +64,30 @@ public class SecurityConfig {
         //            System.out.println("authentication = " + authentication.getName());
         //          }
         //        })
-        .failureHandler(new AuthenticationFailureHandler() {
-          @Override
-          public void onAuthenticationFailure (HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-            System.out.println("exception = " + exception.getMessage());
-            response.sendRedirect("/");
-          }
+        .failureHandler((request, response, exception) -> {
+          System.out.println("exception = " + exception.getMessage());
+          response.sendRedirect("/");
         })
         .permitAll()
       )
-      .logout(httpSecurityLogoutConfigurer -> {
-        httpSecurityLogoutConfigurer
-          .logoutUrl("/logout")
-          .logoutSuccessUrl("/login")
-          .addLogoutHandler(new LogoutHandler() {
-            @Override
-            public void logout (HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-              HttpSession session = request.getSession();
-              session.invalidate();
-            }
-          })
-          .logoutSuccessHandler(new LogoutSuccessHandler() {
-            @Override
-            public void onLogoutSuccess (HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-              response.sendRedirect("/login");
-            }
-          })
-          .deleteCookies("remember-me")
-          .permitAll();
-      })
-      .rememberMe(httpSecurityRememberMeConfigurer -> {
-        httpSecurityRememberMeConfigurer
-          .rememberMeParameter("remember")
-          .tokenValiditySeconds(3600)
-          .userDetailsService(userDetailsService);
-      })
+      .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/login")
+        .addLogoutHandler((request, response, authentication) -> {
+          HttpSession session = request.getSession();
+          session.invalidate();
+        })
+        .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
+        .deleteCookies("remember-me")
+        .permitAll())
+      .rememberMe(httpSecurityRememberMeConfigurer -> httpSecurityRememberMeConfigurer
+        .rememberMeParameter("remember")
+        .tokenValiditySeconds(3600)
+        .userDetailsService(userDetailsService))
+      .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+        .maximumSessions(1)
+        .maxSessionsPreventsLogin(false)
+        .expiredUrl("/login"))
     ;
 
     return http.build();
